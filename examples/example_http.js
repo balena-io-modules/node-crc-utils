064c23433061e8532460644896d78bf8674591d4
Alexis Svinartchouk <alexis@resin.io>
2020-11-13 13:47:42 +0100

    Build with emscripten, more tests, update examples
    Change-type: major

--- examples/example_http.js
+++ examples/example_http.js
@@ -2,46 +2,43 @@ var crc = require('../');
 var zlib = require('zlib');
 
 function createZFullFlush(userBuffer, cb){
-  var buff = [];
-  var gzip = zlib.createGzip({flush:zlib.Z_FULL_FLUSH});
-  gzip.on('data', function(data) {
-    buff.push(data);
-  });
-  gzip.on('end', function(){
-    var concatBuff = Buffer.concat(buff);
-    var len = concatBuff.length;
-    cb({
-      data: concatBuff.slice(10,-10), 
-      len: concatBuff.readUInt32LE(len-4),
-      crc: concatBuff.readUInt32LE(len-8)});
-    });
-  gzip.end(userBuffer);
+	var buff = [];
+	var gzip = zlib.createGzip({flush:zlib.Z_FULL_FLUSH});
+	gzip.on('data', function(data) {
+		buff.push(data);
+	});
+	gzip.on('end', function(){
+		var concatBuff = Buffer.concat(buff);
+		var len = concatBuff.length;
+		cb({
+			data: concatBuff.slice(10,-10),
+			len: concatBuff.readUInt32LE(len-4),
+			crc: concatBuff.readUInt32LE(len-8)});
+	});
+	gzip.end(userBuffer);
 }
 
 var gzipHeader = new Buffer([0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03]);
 var chunksEndBoundary = new Buffer([0x03, 0x00]);
 var chunks = [];
 createZFullFlush(new Buffer('raz '), function(chunk1){
-  createZFullFlush(new Buffer('dwa '), function(chunk2){
-    createZFullFlush(new Buffer('trzy '), function(chunk3){
-      createZFullFlush(new Buffer('BANG!'), function(chunk4){
-        chunks.push(chunk1, chunk2, chunk3, chunk4);
-      });
-    });
-  });
+	createZFullFlush(new Buffer('dwa '), function(chunk2){
+		createZFullFlush(new Buffer('trzy '), function(chunk3){
+			createZFullFlush(new Buffer('BANG!'), function(chunk4){
+				chunks.push(chunk1, chunk2, chunk3, chunk4);
+			});
+		});
+	});
 });
 
 require('http').createServer(function(req, res) {
-    res.writeHead(200, { 'content-type':'text/plain; charset=UTF-8', 'content-encoding': 'gzip' });
-    res.write(gzipHeader);
-    for(var i=0,max=chunks.length;i<max;i++){
-      res.write(chunks[i].data);
-    }
-    res.write(chunksEndBoundary);
-    var data = crc.crc32_combine_multi(chunks);
-    res.write(data.combinedCrc32);
-    res.end(data.bufferLength);
+	res.writeHead(200, { 'content-type':'text/plain; charset=UTF-8', 'content-encoding': 'gzip' });
+	res.write(gzipHeader);
+	for(var i=0,max=chunks.length;i<max;i++){
+		res.write(chunks[i].data);
+	}
+	res.write(chunksEndBoundary);
+	var data = crc.crc32_combine_multi(chunks);
+	res.write(data.combinedCrc32);
+	res.end(data.bufferLength);
 }).listen(8888);
-
-
-
--- test/test.js
+++ test/test.js
@@ -18,11 +18,15 @@ const foobarbaz = Buffer.from('foobarbaz');
 const foobarbazCrc32 = crc32(foobarbaz); // <Buffer 1a 78 27 aa>
 
 describe('combine crc32', () => {
+	before(async () => {
+		await combine.ready;
+	});
+
 	describe('crc32_combine', () => {
 		it('should combine 2 crc32 values', () => {
 			const foobarCrc32Combined = combine.crc32_combine(
-				fooCrc32.readUInt32BE(), 
-				barCrc32.readUInt32BE(), 
+				fooCrc32.readUInt32BE(),
+				barCrc32.readUInt32BE(),
 				bar.length,
 			); // <Buffer 95 1f f6 9e> (Little Endian)
 			assert.equal(foobarCrc32Combined.readUInt32LE(), foobarCrc32.readUInt32BE());
@@ -30,11 +34,38 @@ describe('combine crc32', () => {
 	});
 
 	describe('crc32_combine_multi', () => {
+		it('should throw if anything else than an array is provided', () => {
+			try {
+				const foobarCrc32Combined = combine.crc32_combine_multi('whatever');
+				assert(false);
+			} catch (error) {
+				assert.equal(error.message, combine.message);
+			}
+		});
+
+		it('should throw if the provided array has less than 2 elements', () => {
+			try {
+				const foobarCrc32Combined = combine.crc32_combine_multi([{ crc: 1, len: 1 }]);
+				assert(false);
+			} catch (error) {
+				assert.equal(error.message, combine.message);
+			}
+		});
+
+		it('should throw if crc or len are not integers', () => {
+			try {
+				const foobarCrc32Combined = combine.crc32_combine_multi([{ crc: 'not a number' }, {}]);
+				assert(false);
+			} catch (error) {
+				assert.equal(error.message, combine.message);
+			}
+		});
+
 		it('should combine 2 crc32 values', () => {
 			const foobarCrc32Combined = combine.crc32_combine_multi([
 				{crc: fooCrc32.readUInt32BE(), len: foo.length},
 				{crc: barCrc32.readUInt32BE(), len: bar.length},
-			]); 
+			]);
 			assert.equal(foobarCrc32Combined.intLength, foobar.length);
 			assert.equal(foobarCrc32Combined.combinedCrc32.readUInt32LE(), foobarCrc32.readUInt32BE());
 		});
@@ -44,7 +75,7 @@ describe('combine crc32', () => {
 				{crc: fooCrc32.readUInt32BE(), len: foo.length},
 				{crc: barCrc32.readUInt32BE(), len: bar.length},
 				{crc: bazCrc32.readUInt32BE(), len: baz.length},
-			]); 
+			]);
 			assert.equal(foobarbazCrc32Combined.intLength, foobarbaz.length);
 			assert.equal(foobarbazCrc32Combined.combinedCrc32.readUInt32LE(), foobarbazCrc32.readUInt32BE());
 		});
